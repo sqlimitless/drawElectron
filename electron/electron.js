1c7ee3c7c0aaf278b5ddee42aac6091bef9ac5fd
lhjhoon <lhjhoon@gmail.com>
2022-12-17 12:36:30 +0900

    [Add] AutoUpdater

--- electron/electron.js
+++ electron/electron.js
@@ -1,8 +1,10 @@
 // electron/electron.js
 const path = require('path');
 const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require("electron-updater");
+const log = require('electron-log');
 
-const isDev = process.env.IS_DEV == "true" ? true : false;
+const isDev = process.env.IS_DEV === "true";
 
 function createWindow() {
   // Create the browser window.
@@ -29,11 +31,37 @@ function createWindow() {
   }
 }
 
+/* Updater ======================================================*/
+
+autoUpdater.on('checking-for-update', () => {
+  log.info('업데이트 확인 중...');
+});
+autoUpdater.on('update-available', (info) => {
+  log.info('업데이트가 가능합니다.');
+});
+autoUpdater.on('update-not-available', (info) => {
+  log.info('현재 최신버전입니다.');
+});
+autoUpdater.on('error', (err) => {
+  log.info('에러가 발생하였습니다. 에러내용 : ' + err);
+});
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "다운로드 속도: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - 현재 ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  log.info(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  log.info('업데이트가 완료되었습니다.');
+});
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
+  // 자동 업데이트 등록
+  autoUpdater.checkForUpdates();
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
